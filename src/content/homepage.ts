a8fa70ba0c0af49d5ad95dd166419b36b1f10724
T Prajwal Prabhu <prajwalprabhu.tellar@gmail.com>
2023-02-13 23:19:16 +0530

    initial commit

---
+++ src/content/homepage.ts
@@ -0,0 +1,12 @@
+export const homepage = [
+  {
+    title: 'Bike Riding',
+    content: 'I have a crazy maddness towards bike riding...',
+    icon: 'ic:round-directions-bike',
+  },
+  {
+    title: 'Programing',
+    content: 'Programing is another major madness I have...',
+    icon: 'ph:code-bold',
+  },
+];
